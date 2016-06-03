32bf8dec2c53f18e842987a1edefbc4804caf8a5
Quan Luu <quan@zoomdata.com>
2016-06-03 10:34:02 -0400

    added debug line

--- run-server.sh
+++ run-server.sh
@@ -3,4 +3,4 @@
 # Copyright (C) Zoomdata, Inc. 2012-2016. All rights reserved.
 
 
-java -Dlog4j.configuration=file://`pwd`/log4j.properties  -jar target/edc-server-cratedb-1.0.0.jar 7337
+java -Dlog4j.configuration=file://`pwd`/log4j.properties -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1045 -jar target/edc-server-cratedb-1.0.0.jar 7337
